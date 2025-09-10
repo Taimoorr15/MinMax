db2318cdc3dfe6e681107e8f58e72bf632f5d59d
Taimoorr15 <trafi57@gmail.com>
2025-09-10 21:41:30 +0500

    main

---
+++ main.py
@@ -0,0 +1,5 @@
+from minmax import Min_Max
+
+nums = [5, 2, 9, -3, 7]
+obj = Min_Max(nums)
+print(obj.min_max())  
---
+++ minmax.py
@@ -0,0 +1,16 @@
+class Min_Max:
+    def __init__(self, arr):
+        if not isinstance(arr, list):
+            raise TypeError("arr must be a list")
+        if not all(isinstance(x, (int, float)) for x in arr):
+            raise ValueError("All elements in arr must be numbers")
+        self.arr = arr
+    def min_max(self):
+        max_number = 0
+        min_number = 0
+        for i in range(len(self.arr)):
+            if (self.arr[i]<min_number):
+                min_number = self.arr[i]
+            elif (self.arr[i]>max_number):
+                max_number = self.arr[i]
+        return [min_number,max_number]
